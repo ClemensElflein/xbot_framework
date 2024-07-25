ff88b921d0a53d8b738c0833c9ab257813b2d51d
Clemens Elflein <clemens1@familie-elflein.de>
2024-07-25 17:58:33 +0200

    build libs by default, added possibility to create interfaces without generating a target

--- codegen/cmake/AddService.cmake
+++ codegen/cmake/AddService.cmake
@@ -17,3 +17,20 @@ function(add_service SERVICE_NAME JSON_FILE)
     target_include_directories(${SERVICE_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
     target_link_libraries(${SERVICE_NAME} PUBLIC xbot-service)
 endfunction()
+
+function(target_add_service TARGET_NAME SERVICE_NAME JSON_FILE)
+    add_custom_command(
+            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp
+            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.hpp
+            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.cpp
+            DEPENDS ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.cpp ${JSON_FILE}
+            COMMENT "Generating code for service ${SERVICE_NAME}."
+    )
+
+    target_sources(${TARGET_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp
+            ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp
+    )
+
+    target_include_directories(${TARGET_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
+    target_link_libraries(${TARGET_NAME} PUBLIC xbot-service)
+endfunction()
--- codegen/cmake/AddServiceInterface.cmake
+++ codegen/cmake/AddServiceInterface.cmake
@@ -17,3 +17,21 @@ function(add_service_interface SERVICE_INTERFACE_NAME JSON_FILE)
     target_include_directories(${SERVICE_INTERFACE_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
     target_link_libraries(${SERVICE_INTERFACE_NAME} PUBLIC xbot-service-interface)
 endfunction()
+
+function(target_add_service_interface TARGET_NAME SERVICE_INTERFACE_NAME JSON_FILE)
+
+    add_custom_command(
+            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_INTERFACE_NAME}Base.hpp ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_INTERFACE_NAME}Base.cpp
+            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_INTERFACE_NAME}Base.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceInterfaceTemplate.hpp
+            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_INTERFACE_NAME}Base.cpp ${XBOT_CODEGEN_PATH}/templates/ServiceInterfaceTemplate.cpp
+            DEPENDS ${XBOT_CODEGEN_PATH}/templates/ServiceInterfaceTemplate.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceInterfaceTemplate.cpp ${JSON_FILE}
+            COMMENT "Generating code for service interface ${SERVICE_INTERFACE_NAME}."
+    )
+
+    target_sources(${TARGET_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_INTERFACE_NAME}Base.cpp
+            ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_INTERFACE_NAME}Base.hpp
+    )
+
+    target_include_directories(${TARGET_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
+    target_link_libraries(${TARGET_NAME} PUBLIC xbot-service-interface)
+endfunction()
